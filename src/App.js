bbb7a8710e0296e220d1ba1974e58ff7e92e8e4d
Olha Hanzenko <olhahanzenko@Denyss-MacBook-Air.local>
2023-03-14 11:01:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [tips, setTips]=useState("")
+
+  useEffect(()=>{
+    getTips();
+  },[])
+  const getTips = async () => {
+    const response = await fetch(`http://www.boredapi.com/api/activity/ `);
+    const data = await response.json()
+    console.log(data.activity)
+    setTips(data.activity)
+  }
+
+ return(
+  <div className='app' >
+
+<div className='container'>
+  <h1> Fun Bucket List Ideas</h1>
+</div>
+
+<div className='container'>
+  <p className='about'> A bucket list is a list of experiences one wants to have before they die. It comes from the phrase “kick the bucket”, which means to die. So, it’s a list of things we want to do before we kick the bucket. </p>
+</div>
+
+<div className='container'>
+    <p className='tips'>{tips}</p>
+ </div>
+<div className='container'>
+  <button onClick={getTips}>Let's do some fun!</button>
+</div>
+  </div>
+ )
+  
 }
 
 export default App;
